9ea58bccd6b9a5515395c124dd0bd797f681127d
Tim-Harms <tim.harms97@web.de>
2018-05-11 12:46:49 +0200

    new html templates and css

---
+++ spring-redis-demo/src/main/java/de/hska/lkit/demo/redis/controller/HomeController.java
@@ -0,0 +1,18 @@
+package de.hska.lkit.demo.redis.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+public class HomeController {
+
+    @RequestMapping(value = "/home")
+    public String greetingSubmit(@ModelAttribute Greeting greeting, Model model) {
+        model.addAttribute("home", greeting != null ? greeting : new Greeting());
+        return "home";
+    }
+
+}
