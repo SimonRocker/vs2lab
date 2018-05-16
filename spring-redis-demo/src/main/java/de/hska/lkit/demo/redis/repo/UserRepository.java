2eff49141aad3c9e7570106136538bcb379a9159
Simon Rocker <simon@roecker-gartenarchitekt.de>
2018-05-16 19:51:04 +0200

    Method in interface

--- spring-redis-demo/src/main/java/de/hska/lkit/demo/redis/repo/UserRepository.java
+++ spring-redis-demo/src/main/java/de/hska/lkit/demo/redis/repo/UserRepository.java
@@ -95,4 +95,6 @@ public interface UserRepository {
 	public void follow(String username);
 
 	public Map<String, Follower_Relation> getAllRelations();
+
+	public List<String> getFollowedUsersForCurrentUser();
 }
